f321560db917c82fc9eafaf4572eb9a530f0cc2a
cheick18 <97468180+cheick18@users.noreply.github.com>
2024-08-09 15:47:50 +0100

    solution pour le web view 1.0.0

--- index.js
+++ index.js
@@ -107,6 +107,69 @@ const querySnapshot = await getDocs(mappingTableRef);
       res.status(500).send('Erreur lors de la récupération');
     }
   });
+  function getJiraAuthUrl() {
+
+    //return 'https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=hlNe3kROVgzSkZBsPo60j7A9lfdHJGjl&scope=manage%3Ajira-project%20manage%3Ajira-configuration%20read%3Ajira-user%20write%3Ajira-work%20manage%3Ajira-webhook%20manage%3Ajira-data-provider%20read%3Ajira-work&redirect_uri=https%3A%2F%2Fexpensemobileapp-2.onrender.com%2Fsignin-jira&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent'
+  
+   return  `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${process.env.JIRA_ID}&scope=read%3Ame%20read%3Aaccount&redirect_uri=https%3A%2F%2Fexpensemobileapp-2.onrender.com%2Fsignin-jira&state=YOUR_USER_BOUND_VALUE&response_type=code&prompt=consent`;
+    //return 'https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=oQ1Cp0ZVBnU3FizZcbJ6x3OmFfylph9G&scope=read%3Ame%20read%3Aaccount&redirect_uri=https%3A%2F%2F4037-41-141-191-127.ngrok-free.app%2Fsignin-jira&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent' 
+    
+   }
+  async function exchangeJiraCodeForToken(code) {
+    console.log("le code est ")
+
+
+    try {
+    
+      const tokenResponse = await axios.post('https://auth.atlassian.com/oauth/token', {
+        code,
+       redirect_uri: 'https://expensemobileapp-2.onrender.com/signin-jira',
+      client_id: process.env.JIRA_ID,
+      client_secret: process.env.JIRA_SECRET,
+        grant_type: 'authorization_code',
+      });
+  
+      const accessToken = tokenResponse.data.access_token;
+  
+      const userDataResponse = await axios.get('https://api.atlassian.com/me', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      if (userDataResponse.status === 200) { 
+        const userData = userDataResponse.data;
+        console.log("la reponse est", userData);
+        return userData;
+      } else {
+        console.log("échec de récupération des données !");
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'échange du code contre le token :', error.message);
+      throw error;
+    }
+  }
+
+
+
+  app.get('/signin-jira', async (req, res) => {
+    try {
+     
+      if (req.query.code) {
+        const code = req.query.code;
+        const tokenResponse = await exchangeJiraCodeForToken(code);
+      //  const accessToken = response.data.access_token;
+        res.redirect(`com.waga.janngamobileapp:/oauthredirect?access_token=${tokenResponse}`);
+      //  res.send(tokenResponse);
+      } else {
+     
+        const authUrl =  getJiraAuthUrl();
+        res.redirect(authUrl);
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'échange du code contre le token :', error);
+      res.status(500).send('Erreur lors de l\'échange du code contre le token');
+    }
+  });
 
   app.listen(port, () => {
     console.log(`Serveur démarré sur http://localhost:${port}`);
